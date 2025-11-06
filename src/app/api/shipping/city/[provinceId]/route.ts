e2e03e30c56fb8870c661459d2a1deb19b34039d
Yusup Juniadi <yusup.juniadi@outlook.com>
2025-11-07 00:37:22 +0700

    fixing vercel

--- src/app/api/shipping/city/[provinceId]/route.ts
+++ src/app/api/shipping/city/[provinceId]/route.ts
@@ -3,8 +3,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 const API_KEY = "p26fMbua8cec38f853fecb50BTmJIFKj";
 
-export async function GET(req: NextRequest, { params }: { params: { provinceId: string } }) {
-  const provinceId = params.provinceId;
+type Params = { provinceId: string };
+
+export async function GET(req: NextRequest, context: { params: Params }) {
+  const { provinceId } = context.params;
 
   if (!provinceId || isNaN(Number(provinceId))) {
     return NextResponse.json({ success: false, error: "Invalid Province ID" }, { status: 400 });
